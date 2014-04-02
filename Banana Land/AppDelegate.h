4db04e9104f3884fa1f15fb8a27af554f3780908
Khomkrid Lerdprasert <aofiee@Khomkrids-MacBook.local>
2014-04-02 17:53:07 +0700

    Initial Commit

---
+++ Banana Land/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Banana Land
+//
+//  Created by Khomkrid Lerdprasert on 4/2/2557 BE.
+//  Copyright Twin Synergy Co., Ltd. 2557. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Banana Land/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Banana Land
+//
+//  Created by Khomkrid Lerdprasert on 4/2/2557 BE.
+//  Copyright Twin Synergy Co., Ltd. 2557. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
